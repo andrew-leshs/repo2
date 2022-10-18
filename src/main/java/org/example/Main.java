14e0e8fc5ffd64143602643ceb50c317deecbc1a
Your Name <you@example.com>
2022-10-18 17:00:32 +0300

    commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        IntsCalculator intsCalculator = new IntsCalculator();
+        System.out.println(intsCalculator.sum(2, 3));
+
+    }
+}
